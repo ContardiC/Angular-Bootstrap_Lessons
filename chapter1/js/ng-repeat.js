7e756a3707f7ff707ed88dceda89b225be47d00a
ContardiC <c.contardi@icloud.com>
2018-03-21 19:48:38 +0100

    isHidden Button CHECK

---
+++ chapter1/js/ng-repeat.js
@@ -0,0 +1,14 @@
+function contactCtrl($scope){
+    $scope.contacts=[
+        {
+            name:'Jonh Doe',
+            phone:'0123456789',
+            email:'john@example.com'
+        },
+        {
+            name: 'Carlo Contardi',
+            phone:'9876543210',
+            email: 'carlo@example.com'
+        }
+    ];
+}
